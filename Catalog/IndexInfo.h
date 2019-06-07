f6952da6a24fd1402f57d19366d8a6297d9649f6
QinJiuJiu <48308150+QinJiuJiu@users.noreply.github.com>
2019-06-08 03:30:35 +0800

    Create IndexInfo.h

---
+++ Catalog/IndexInfo.h
@@ -0,0 +1,20 @@
+#ifndef IndexManager_h 
+#define IndexManager_h
+#include <string>
+#include <stdio.h>
+#include <map>
+#include <sstream>
+
+using namespace std;
+class IndexInfo
+{
+public:
+    string indexName;
+    string tableName;
+    string Attribute;
+    int type;
+
+    IndexInfo(string indexName, string tableName, string Attribute, int type) : indexName(indexName), tableName(tableName), Attribute(Attribute), type(type) {}
+};
+
+#endif
