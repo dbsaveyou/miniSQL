0d250261a4d4b475430935c26ca40c1a7319d40c
ZhuYuanMing <51505179+ZhuYuanMing@users.noreply.github.com>
2019-06-17 04:35:28 +0800

    Update IndexManager.h

--- Index/IndexManager.h
+++ Index/IndexManager.h
@@ -5,20 +5,11 @@
 #include <stdio.h>
 #include <map>
 #include <sstream>
-#include "bplusself.h"
-#include "basic.h"
+#include "BPlusTree.h"
+#include "Basic.h"
+//#include "IndexInfo.h"
 using namespace std;
 
-class IndexInfo
-{
-private:
-    string indexName;
-    string tableName;
-    string Attribute;
-    int type;
-public:
-    IndexInfo(string indexName,string tableName,string Attribute,int type) : indexName(indexName), tableName(tableName), Attribute(Attribute), type(type){}
-};
 
 class API;
 
@@ -30,10 +21,11 @@ private:
     //
     int static const TYPE_FLOAT = -1;
     int static const TYPE_INT = 0;
+    static const int BLOCK_SIZE = 4096;
     // other values mean the size of the char.Eg, 4 means char(4);
-    
+
     API *api; // to call the functions of API
-    
+
     intMap indexIntMap;
     stringMap indexStringMap;
     floatMap indexFloatMap;
@@ -42,25 +34,26 @@ private:
         float floatTmp;
         string stringTmp;
     }; // the struct to help to convert the inputed string to specfied type
-
     struct keyTmp keyTmps;
-    int getDegree(int type);
-    int getKeySize(int type);  
-    void setKey(int type,string key);
-    
-    
+
+
 public:
-    IndexManager(API *api);
+    //IndexManager(API *api);
+    IndexManager();
     ~IndexManager();
     void createIndex(string filePath,int type);
     void dropIndex(string filePath,int type);               //need type
-    int searchIndex(string filePath,string key,int type);       
+    int searchIndex(string filePath,string key,int type);
     void insertIndex(string filePath,string key,int blockOffset,int type);
     void deleteIndex(string filePath, string key,int type);
-
-    void Create_Index(Create_Index tmp2);
-    void Delete_index(Drop_index tmp3);
-
-
+    int getDegree(int type);
+    int getKeySize(int type);
+    void setKey(string key, int type);
+    
+    void CreateTable(Create_Table table);
+    void DropTable(Drop_Table table);
+    void Create_Index(Create_Index index);
+    void Delete_Index(Drop_Index index);
+    
 };
 #endif
